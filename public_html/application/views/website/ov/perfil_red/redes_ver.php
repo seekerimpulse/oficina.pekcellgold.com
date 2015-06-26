30555b27775fdc5ee9a903d085fea0a82f4229ec
PC <gianci15@ingenieros.com>
2015-06-26 15:49:12 -0500

    Subir Backlog

--- public_html/application/views/website/ov/perfil_red/redes_ver.php
+++ public_html/application/views/website/ov/perfil_red/redes_ver.php
@@ -1,4 +1,18 @@
-
+ <!-- MAIN CONTENT -->
+<div id="content">
+	<div class="row">
+		<div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+			<h1 class="page-title txt-color-blueDark">
+					<a href="/bo"><i class="fa fa-home"></i> Menu</a>
+				<span>> <a href="/bo/configuracion/"> Configuracion </a>
+				> <a href="/bo/configuracion/tipoRed"> Tipo De Red </a>
+				>	Listar
+				</span>
+			</h1>
+		</div>
+	</div>
+<section id="widget-grid" class="">
+<div class="row" style="background: rgb(255, 255, 255) none repeat scroll 0% 0%;margin-bottom:2rem;">	
 <div class="well">
 		<fieldset>
 											<legend>Red</legend>
@@ -14,3 +28,6 @@
 												<?php } ?>
 			</fieldset>
 	</div>
+</div>
+</section>	
+</div>	
