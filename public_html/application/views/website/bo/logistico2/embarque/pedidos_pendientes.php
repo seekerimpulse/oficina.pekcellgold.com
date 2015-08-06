b2d3dfd3895d887783d98c14aa478e9e6d1b7308
PC <edixon.hernandez.c@gmail.com>
2015-08-06 12:11:32 -0500

    corrcion logistico

--- public_html/application/views/website/bo/logistico2/embarque/pedidos_pendientes.php
+++ public_html/application/views/website/bo/logistico2/embarque/pedidos_pendientes.php
@@ -5,7 +5,7 @@
 						<h1 class="page-title txt-color-blueDark">
 								<a class="backHome" href="/bo"><i class="fa fa-home"></i> Menu</a>
 							<span>
-								> <a href="/bo/logistico2">Modulo log&iacute;stico</a> 
+								> <a href="/bo/logistico2">Log&iacute;stico</a> 
 								> <a href="/bo/logistico2/pedidos">Pedidos / Embarques</a>
 								> Pedidos Pendientes
 							</span>
