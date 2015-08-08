878e0c9a684999e7e0f278d0890a58eef73ad2e9
PC <edixon.hernandez.c@gmail.com>
2015-08-08 12:16:40 -0500

    correciones finales

--- public_html/cometchat/model_soporte_chat.php
+++ public_html/cometchat/model_soporte_chat.php
@@ -5,14 +5,14 @@
 
 	
 		function red_a_red(){
-			setcookie("red1","1", time() + (86400)*30, "/");
+			setcookie("red1","1", time() + (86400)*365, "/");
 		}
 		
 		function red_soporte(){
-			setcookie("red1","2", time() + (86400)*30, "/");
+			setcookie("red1","2", time() + (86400)*365, "/");
 		}
 		function soporte_red(){
-			setcookie("red1","3", time() + (86400)*30, "/");
+			setcookie("red1","3", time() + (86400)*365, "/");
 		}
 	
 	
